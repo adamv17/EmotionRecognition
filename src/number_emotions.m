4670e0e6ccb47810f7460f5ed2dcb5f3a32f1355
Adam <adam.vichik@gmail.com>
2020-07-12 10:21:09 +0300

    Change number_emotions function to work for all databases

--- src/number_emotions.m
+++ src/number_emotions.m
@@ -1,23 +1,8 @@
-function [num_emotions] = number_emotions(emotions)
+function [num_emotions] = number_emotions(emotions, labels)
 %UNTITLED Summary of this function goes here
 %   Detailed explanation goes here
-num_emotions = zeros(length(emotions),1);
-for e = 1:length(emotions)
-    switch (string(emotions(e)))
-        case "anger"
-            num_emotions(e) = 1;
-        case "boredom"
-            num_emotions(e) = 2;
-        case "disgust"
-            num_emotions(e) = 3;
-        case "fear"
-            num_emotions(e) = 4;
-        case "happy"
-            num_emotions(e) = 5;
-        case "sad"
-            num_emotions(e) = 6;
-        case "neutral"
-            num_emotions(e) = 7;
-    end
+num_emotions = zeros(size(emotions, 2), 1);
+for e = 1:numel(num_emotions)
+    num_emotions(e, 1) = find(contains(labels, emotions(e)));
 end
 
