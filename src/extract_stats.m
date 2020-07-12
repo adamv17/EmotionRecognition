af2751b1f262d87f1cbc5752e2bbbac06e9d645c
Adam <adam.vichik@gmail.com>
2020-07-12 10:17:56 +0300

    Fix extract_stats

--- src/extract_stats.m
+++ src/extract_stats.m
@@ -4,7 +4,8 @@ function [stats,stat_names] = extract_stats(features, stats2extract, num_emotion
 num_of_stats = size(stats2extract, 2);
 stats = zeros(1 + num_of_stats * num_of_features, size(features, 2));
 for f = 1:size(features, 2)
-    [st, stat_names] = statistics(features{1,f}, stats2extract);
+    seg_features = features(:,f);
+    [st, stat_names] = statistics(seg_features, stats2extract, num_of_features);
     stats(1:end-1, f) = st(:);
     stats(end, f) = num_emotions(f,1);
 end
